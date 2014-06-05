370720be23f9e20c6db71a78a29e62d808b45b41
Rahul Patil <rahulpatil2387@gmail.com>
2014-06-05 09:18:26 +0530

    Removed Active admin and done some changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-    before_filter :authenticate_user!
+    before_filter :authenticate_user! ,:except => [:index]
   def index
     @articles = Article.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Blog::Application.routes.draw do
   
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-
-  devise_for :users
-  ActiveAdmin.routes(self)
+devise_for :users
 
 #  root :to => "home#index"
 
