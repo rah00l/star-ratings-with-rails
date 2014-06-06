5a6d3e47fefd836864391760b751023582ed617d
Rahul Patil <rahulpatil2387@gmail.com>
2014-06-06 21:08:42 +0530

    Disable the asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -51,7 +51,7 @@ module Blog
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
 
     config.assets.initialize_on_precompile = false
 
