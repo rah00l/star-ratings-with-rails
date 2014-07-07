cae4e703765849465c7d4b4089eed452da4b4ef2
Rahul Patil <rahulpatil2387@gmail.com>
2014-07-07 19:32:38 +0530

    collection_select updated

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ActiveRecord::Base
-   attr_accessible :name, :street_address, :landmark, :pincode, :phoneno
+   attr_accessible :name, :street_address, :landmark, :pincode, :phoneno, :country_id
   belongs_to :user
   belongs_to :country
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140707094438) do
+ActiveRecord::Schema.define(:version => 20140707104610) do
 
   create_table "addresses", :force => true do |t|
     t.string   "name"
@@ -149,6 +149,14 @@ ActiveRecord::Schema.define(:version => 20140707094438) do
   add_index "rs_reputations", ["reputation_name", "target_id", "target_type"], :name => "index_rs_reputations_on_reputation_name_and_target", :unique => true
   add_index "rs_reputations", ["reputation_name"], :name => "index_rs_reputations_on_reputation_name"
   add_index "rs_reputations", ["target_id", "target_type"], :name => "index_rs_reputations_on_target_id_and_target_type"
+
+  create_table "states", :id => false, :force => true do |t|
+    t.string   "name"
+    t.string   "code"
+    t.integer  "country_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
