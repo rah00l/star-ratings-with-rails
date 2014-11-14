95dea55c6332f7617f1e01f33dc5808b2ef05615
Rahul Patil <rahulpatil2387@gmail.com>
2014-11-14 19:56:48 +0530

    added flash message

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,8 +3,6 @@ class RatingsController < ApplicationController
   def update
     @rating = Rating.find(params[:id])
     @article = @rating.article
-    # flash.now[:notice] =  "Thank you for rating!"
-
     if @rating.update_attributes(score: params[:score])
 		respond_to do |format|
       	 format.js {  flash[:notice] = "Thank you for rating!" }
