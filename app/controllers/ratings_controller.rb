19c22b0cbdd1572a2e330e62e722e3f29ea7808f
Rahul <rahulpatil2387@gmail.com>
2014-11-14 13:53:39 +0530

    vote up - down icon got changed

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,10 +3,11 @@ class RatingsController < ApplicationController
   def update
     @rating = Rating.find(params[:id])
     @article = @rating.article
-    
+    # flash.now[:notice] =  "Thank you for rating!"
+
     if @rating.update_attributes(score: params[:score])
-      respond_to do |format|
-        format.js
+		respond_to do |format|
+      	 format.js {  flash[:notice] = "Thank you for rating!" }
       end
     end
   end
