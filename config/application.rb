dd071faec48a154435e5773241e6733f1e4a302f
Rahul <rahulpatil2387@gmail.com>
2014-11-17 11:25:47 +0530

    Added Action::Mailer configuration for sending mail from gmail account.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,19 @@ end
 
 module Blog
   class Application < Rails::Application
+
+
+    ActionMailer::Base.delivery_method = :sendmail
+
+    ActionMailer::Base.sendmail_settings = {
+        :address              => "smtp.gmail.com",
+        :port                 => "587",
+        :domain               => "gmail.com",
+        :user_name            => "xxx@gmail.com",
+        :password             => "yyy",
+        :authentication       => "plain",
+        :enable_starttls_auto => true
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,21 +37,21 @@ Blog::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["DOMAIN_NAME"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: ENV["DOMAIN_NAME"],
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV["GMAIL_USERNAME"],
+  #   password: ENV["GMAIL_PASSWORD"]
+  # }
+
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   # Send email in development mode.
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
 
-  
 end
