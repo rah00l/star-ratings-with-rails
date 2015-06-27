c359011b239a29753ece5ca4dadf873e96938c02
Rahul Patil <rahulpatil2387@gmail.com>
2015-06-28 03:26:08 +0530

    Added factories file with couple of facories 	- also added it's configurations in test_helper.rb         - enables transactional_fictures to use.         - and devise test-helpers also included

---
+++ test/factories.rb
@@ -0,0 +1,38 @@
+# Factory Girl Definitions
+
+# sugar for time-stamp as string
+# def dts_string
+#   FactoryGirl.generate(:timestamp).strftime("%Y-%m-%d_%H-%M-%S")
+# end
+
+FactoryGirl.define do
+
+  sequence :email do |n|
+    "test-#{n}@mail.com"
+  end
+
+  factory :user do |f|
+    f.email { FactoryGirl.generate(:email) }
+    f.password "12345678"
+  end
+
+  # for generating time-stamps
+  sequence :timestamp do |n|
+    (rand*n).days.ago
+  end
+
+  # for generating names
+  sequence :name do |n|
+    "Article-#{n}"
+  end
+
+  # created article's basic factory stucture
+  factory :article do |f|
+    f.name          { FactoryGirl.generate(:name) }
+    f.content      "This guide is designed for beginners who want to get started with a Rails application."
+    f.created_at  { FactoryGirl.generate(:timestamp) }
+    f.updated_at     nil
+    f.published_at   nil
+  end
+end
+# name: "Batman", created_at: (rand*30).days.ago, content:
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,12 +2,17 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require 'factory_girl_rails'
+
+class ActionController::TestCase
+  include Devise::TestHelpers
+end
+
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
+  # Transactional fixtures accelerate your tests by wrapping each test method
+    # in a transaction that's rolled back on completion.  This ensures that the
+    # test database remains unchanged
+    self.use_transactional_fixtures = true
 
   # Add more helper methods to be used by all tests here...
 end
