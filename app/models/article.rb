96a1c0d4f36cdaffd898e85a51d042cd623952d8
Rahul Patil <rahulpatil2387@gmail.com>
2015-06-28 03:14:46 +0530

    Added schema-info in article model and proprly indented

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,16 @@
 class Article < ActiveRecord::Base
+
+  ############################# Scema Information ##############################################################
+  #   Column        Type          Collation            Null   Default     Extra
+  # 1   id            int(11)                             No      None        AUTO_INCREMENT
+  # 2   name          varchar(255)    utf8_unicode_ci     Yes
+  # 3   content       text            utf8_unicode_ci     Yes
+  # 4   created_at    datetime                            No      None
+  # 5   updated_at    datetime                            No      None
+  # 6   user_id       int(11)                             Yes     NULL
+  # 7   published_at  datetime                            Yes     NULL
+  ############################## Scema Information #############################################################
+
   attr_accessible :content, :name , :published_at, :created_at, :tag_list
   has_many :comments, as: :commentable, :dependent => :destroy
   has_many :ratings
@@ -11,7 +23,7 @@ class Article < ActiveRecord::Base
 
   acts_as_taggable
 
-  # Validation 
+  # Validation
   validates :name, presence: true, uniqueness: true
   validates :content, presence: true
 
@@ -30,7 +42,7 @@ class Article < ActiveRecord::Base
   def published_month
     published_at.strftime("%B %Y") unless published_at.blank?
   end
-  
+
   def average_rating
     ratings.size.zero? ? 0 : ratings.sum(:score) / ratings.size
   end
@@ -39,13 +51,13 @@ class Article < ActiveRecord::Base
     published_at.strftime('%d %B %Y %H:%M:%S') unless published_at.blank?
   end
 
-private
+  private
 
   def set_published_at
     value = (self.created_at + 1.day) unless self.created_at.nil?
     self.update_attributes(:published_at => value)
-  end  
+  end
 
-  
+
 
 end
