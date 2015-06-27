51e32762b023601af08918f84abcf0b1ded113fd
Rahul Patil <rahulpatil2387@gmail.com>
2015-06-28 00:01:39 +0530

    Improvement Plan #2 - Removed  generated empty testcases and factories for individual files

--- spec/factories/articles.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :article do 
-  		name 'Test Article'
-  		content 'The best way to use this guide is to follow each step as it happens, no code or step needed to make this example application has been left out, so you can literally follow along step by step. You can get the complete code'
-		# sequence(:name) { |n| "Test Article#{n}"}
-	end
-end
