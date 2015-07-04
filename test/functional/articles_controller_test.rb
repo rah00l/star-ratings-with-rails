6a3cce0899c5c422a71b6f46aad06b171602aca0
Rahul Patil <rahulpatil2387@gmail.com>
2015-07-04 14:34:42 +0530

    Improvement Plan #3 - Restructured test cases with new descriptive type of styling (like Shoulda )

--- test/functional/articles_controller_test.rb
+++ test/functional/articles_controller_test.rb
@@ -8,47 +8,45 @@ class ArticlesControllerTest < ActionController::TestCase
     @user = FactoryGirl.create(:user)
     # @response   = ActionController::TestResponse.new
     sign_in @user
+    should "get index" do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:articles)
+    end
+
+    should "get new" do
+      get :new
+      assert_response :success
+    end
+
+    should "create article" do
+      article = FactoryGirl.build(:article)
+      post :create, :article => article.attributes
+      assert_response :redirect
+      assert_equal 'Article was successfully created.', flash[:notice]
+    end
+
+    should "show article" do
+      get :show, id: @article
+      assert_response :success
+    end
+
+    should "get edit" do
+      get :edit, id: @article
+      assert_response :success
+    end
+
+    should "update article" do
+      put :update, id: @article, article: { content: @article.content, name: @article.name }
+      assert_redirected_to articles_path
+    end
+
+    should "destroy article" do
+      assert_difference('Article.count', -1) do
+        delete :destroy, id: @article
+      end
+
+      assert_redirected_to articles_path
+    end
   end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:articles)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  # test "should create article" do
-  #   assert_difference('Article.count') do
-  #     post :create, article: { content: @article.content, name: @article.name }
-  #   end
-
-  #   assert_redirected_to article_path(assigns(:article))
-  # end
-
-  # test "should show article" do
-  #   get :show, id: @article
-  #   assert_response :success
-  # end
-
-  # test "should get edit" do
-  #   get :edit, id: @article
-  #   assert_response :success
-  # end
-
-  # test "should update article" do
-  #   put :update, id: @article, article: { content: @article.content, name: @article.name }
-  #   assert_redirected_to article_path(assigns(:article))
-  # end
-
-  # test "should destroy article" do
-  #   assert_difference('Article.count', -1) do
-  #     delete :destroy, id: @article
-  #   end
-
-  #   assert_redirected_to articles_path
-  # end
 end
