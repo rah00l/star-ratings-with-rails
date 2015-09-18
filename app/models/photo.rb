acc36222b38ff9bbd0dadb009c61ec255ef3498e
Rahul Patil <rahulpatil2387@gmail.com>
2015-09-19 00:48:17 +0530

    IP-27 - Updates photo.rb file with related schema info

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,13 @@
 class Photo < ActiveRecord::Base
+
+ # #	Column		Type		Collation				Null	Default	Extra	Action
+ # 1	id			int(11)								No			None	AUTO_INCREMENT	 
+ # 2	name		varchar(255)	utf8_unicode_ci		Yes			NULL		 
+ # 3	filename	varchar(255)	utf8_unicode_ci		Yes			NULL	
+ # 4	created_at	datetime							No			None
+ # 5	updated_at	datetime							No			None
+ # 6	user_id		int(11)								Yes			NULL
+
   attr_accessible :filename, :name, :attachments_attributes
 
   has_many :comments, as: :commentable, :dependent => :destroy
