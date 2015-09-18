b5dc91ac34cc8fe099339df43da6361cb68cbbe9
Rahul Patil <rahulpatil2387@gmail.com>
2015-09-19 02:05:23 +0530

    IP-27 - Added schema info in attachment and music files

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,4 +1,16 @@
 class Attachment < ActiveRecord::Base
+
+  # ############################# Scema Information ##############################################################
+  #	Column				Type		Collation				Null		Default
+  # 1	id				int(11)								No	None	AUTO_INCREMENT
+  # 2	file			varchar(255)	utf8_unicode_ci		Yes	NULL
+  # 3	attachable_id	int(11)								Yes	NULL
+  # 4	attachable_type	varchar(255)	utf8_unicode_ci		Yes	NULL
+  # 5	user_id			int(11)								Yes	NULL
+  # 6	created_at		datetime							No	None
+  # 7	updated_at		datetime							No	None
+  # ############################# Scema Information ##############################################################
+
   attr_accessible :file, :attachable_type, :attachable_id, :user_id
 
   mount_uploader :file, FileUploader
--- app/models/music.rb
+++ app/models/music.rb
@@ -1,4 +1,17 @@
 class Music < ActiveRecord::Base
+
+# ############################# Scema Information ##############################################################
+ #	Column			Type		Collation				Null		Default
+ # 1	id			int(11)								No			AUTO_INCREMENT
+ # 2	title		varchar(255) utf8_unicode_ci		Yes			NULL
+ # 3	description	text		utf8_unicode_ci			Yes			NULL
+ # 4	featured	tinyint(1)							Yes			NULL
+ # 5	summary		text	utf8_unicode_ci				Yes			NULL
+ # 6	user_id		int(11)								Yes			NULL
+ # 7	created_at	datetime							No			None
+ # 8	updated_at	datetime							No			None
+ #  ############################# Scema Information ##############################################################
+
   attr_accessible :description, :featured, :summary, :title, :user_id, :attachments_attributes
 
   has_many :attachments, as: :attachable, :dependent => :destroy
